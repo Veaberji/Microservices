c543ea01d72cd9f0aaa1251045a50f4a5f82b703
Pavel Morozov <veaberji@gmail.com>
2023-10-09 17:04:56 +0400

    create CommandsService

---
+++ CommandsService/CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CommandsController : ControllerBase
+{
+}
---
+++ CommandsService/CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers;
+
+[Route("api/commands/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    [HttpPost("Test")]
+    public string Test()
+    {
+        return "a";
+    }
+}
